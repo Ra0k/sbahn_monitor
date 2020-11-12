116f11b797186f1153e8e3219f510aaeea2dc333
David Szabo <david.szabo@shore.com>
2020-11-12 23:04:18 +0100

    add station names for S8 line

---
+++ crawler/stations.py
@@ -0,0 +1,34 @@
+S8_stops = """Herrsching
+Seefeld-Hechendorf
+Steinebach
+Weßling
+Neugilching
+Gilching-Argelsried
+Geisenbrunn
+Germering-Unterpfaffenhofen
+Harthaus
+Freiham
+Neuaubing
+Westkreuz
+Pasing
+Laim
+Hirschgarten
+Donnersbergerbrücke
+Hackerbrücke
+Hauptbahnhof
+Karlsplatz
+Marienplatz
+Isartor
+Rosenheimer Platz
+Ostbahnhof München
+Leuchtenbergring
+Daglfing
+Englschalking
+Johanneskirchen
+Unterföhring
+Ismaning
+Hallbergmoos
+Flughafen Besucherpark
+Flughafen München""".split('\n')
+
+STATIONS = list(set(S8_stops))
