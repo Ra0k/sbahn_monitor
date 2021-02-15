a941215705ecc054fc93f5a8e75ec30ba4bb25a1
mariampillai <mariampillai@tufast.de>
2021-02-15 14:37:52 +0100

    Multiselect fix

--- client/sbahn-monitor/src/views/Routing.vue
+++ client/sbahn-monitor/src/views/Routing.vue
@@ -120,31 +120,14 @@ export default{
 </script>
 
 
-<style>
+<style src="vue-multiselect/dist/vue-multiselect.min.css">
+  /* ul{
+    overflow: scroll;
+    height: 300px;
+  }
 
-nav {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-nav .menu-item {
-  color: #FFF;
-  padding: 10px 20px;
-  position: relative;
-  text-align: center;
-  border-bottom: 3px solid transparent;
-  display: flex;
-  transition: 0.4s;
-}
-nav .menu-item.active,
-nav .menu-item:hover {
-  background-color: #444;
-  border-bottom-color: #FF5858;
-}
-nav .menu-item a {
-  color: inherit;
-  text-decoration: none;
-}
-
-
+  li{
+    list-style: none;
+    list-style-type: none;
+  } */
 </style>
